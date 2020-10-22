6e6a36e8c330100ac40ed9528d14551e36e9115d
aydar sadrutdinov <Ajdar18@yandex.ru>
2020-10-22 10:14:45 +0400

    deploy v 9

--- src/main/java/bot/TelegramBotApplication.java
+++ src/main/java/bot/TelegramBotApplication.java
@@ -4,7 +4,6 @@ package bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
 import org.telegram.telegrambots.ApiContextInitializer;
 
 
